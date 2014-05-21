21fabdc8439b0002b850d7af66e4a7fcd183b4a6
majidmak <majukiller@gmail.com>
2014-05-20 23:45:45 -0700

    majid
    majid

--- task.done/Task.done/src/com/gravity/innovation/task/done/NavigationDrawerFragment.java
+++ task.done/Task.done/src/com/gravity/innovation/task/done/NavigationDrawerFragment.java
@@ -27,6 +27,8 @@ import android.widget.Toast;
  * See the <a href="https://developer.android.com/design/patterns/navigation-drawer.html#Interaction">
  * design guidelines</a> for a complete explanation of the behaviors implemented here.
  */
+
+//msjid commented 
 public class NavigationDrawerFragment extends Fragment {
 
     /**
