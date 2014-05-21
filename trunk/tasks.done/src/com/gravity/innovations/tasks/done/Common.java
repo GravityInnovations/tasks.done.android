8092ef2c84459ef78fd70f907abd4a3688795973
Faik Malik <faik.malik89@gmail.com>
2014-05-21 14:30:36 +0500

    - Added potentially required permissions in {AndroidManifest.xml} - Added PublicKeys Class with usuage comments

---
+++ trunk/tasks.done/src/com/gravity/innovations/tasks/done/Common.java
@@ -0,0 +1,13 @@
+package com.gravity.innovations.tasks.done;
+//This class will include common keys used in application
+
+//default decleration as below
+//public static final int MY_KEY_SOME = 0;
+
+//default usuage outside as below
+//PublicKeys.MY_KEY_SOME
+//pass it to switch, bundle extras, shared prefs etc
+public class Common {
+	public static final int USER_EMAIL = 1;//used in shared prefs for io of user email address
+
+}
