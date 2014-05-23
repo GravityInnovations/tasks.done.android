2837ef3878142b3b230379dc4bc3e0414cafe9f3
mushahidhassan <mushahidhassan110@gmail.com>
2014-05-23 06:12:31 -0700

    changes in common file

--- trunk/tasks.done/src/com/gravity/innovations/tasks/done/Common.java
+++ trunk/tasks.done/src/com/gravity/innovations/tasks/done/Common.java
@@ -53,6 +53,7 @@ public class Common {
 	public static final String NETWORK_ERROR = "NetworkError";
 	public static final int one = 2;
 	public static final int two = 1;//mushahid
+	public static final int three = 1;//mushahid
 	// commands - Splash
 	public static final int CHECK_INTERNET = 1;
 	public static final int LOAD_PREFS = 2;
